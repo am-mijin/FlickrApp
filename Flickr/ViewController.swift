3ec905975738b32c669e3d97281c2f32e962c4db
mijinatdiscovery <mijin_cho@discovery.com>
2018-01-26 19:22:37 +0000

    Minor UI fix Change file location

--- Flickr/ViewController.swift
+++ Flickr/ViewController.swift
@@ -8,9 +8,8 @@
 
 import UIKit
 import RxSwift
-
+import RxCocoa
 class ViewController: UIViewController {
-   
     let feeds: Variable<[Feed]> = Variable([])
     private let disposeBag = DisposeBag()
     lazy var flickrHTTPServiceProvider: HTTPServiceProvider = {
@@ -34,53 +33,15 @@ class ViewController: UIViewController {
        
         getFeeds()
             .subscribe(onNext: { [weak self] feeds in
-                        self?.feeds.value = feeds },
-                       onError: { error in print(error)
-            }).disposed(by: disposeBag)
-    }
-    
-    private func getFeeds() -> Observable<[Feed]> {
-        return flickrHTTPServiceProvider.data(fromUrlString: "https://api.flickr.com/services/feeds/photos_public.gne?tags=food&tagmode=any&format=json&nojsoncallback=1")
-            .timeout(30, scheduler: MainScheduler.instance)
-            .map { data in
-                guard let invalidData = data as? Data else {
-                    throw HTTPServiceProviderError.unhandledResponse
-                }
-                // Flickr JSON output is invalid. To make it valid, remove "\\"
-                let invalidDataStr = String(data: invalidData, encoding: .utf8)
-                let validDataStr = invalidDataStr?.replacingOccurrences(of: "\\'", with: "'")
-                guard let jsonData = validDataStr?.data(using: .utf8) else{
-                    throw HTTPServiceProviderError.unhandledResponse
-                }
-                let jsonStr = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any]
-                
-                guard let json = jsonStr else {
-                    throw HTTPServiceProviderError.unhandledResponse
-                }
-                guard let items = json ["items"] as? [Any] else {
-                    throw HTTPServiceProviderError.unhandledResponse
-                }
-                var feeds = [Feed]()
-                for item in items {
-                    guard let dictionary = item as? [String : Any] else {
-                        throw HTTPServiceProviderError.unhandledResponse
-                    }
-                    let feed = Feed(dictionary: dictionary)
-                    feeds.append(feed)
-                }
-                return feeds
-            }
-           .do( //onNext: {  [weak self]  feeds in
-//                },
-                onError: { error in
-                    print(error.localizedDescription)
-                })
-            .catchErrorJustReturn([])
+                   self?.feeds.value = feeds},
+                   onError: { error in print(error)})
+            .disposed(by: disposeBag)
     }
 }
 
 extension ViewController {
     private func setup() {
+        
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 300
         tableView.layoutMargins = UIEdgeInsets.zero
@@ -89,23 +50,23 @@ extension ViewController {
     
     private func setupCellConfiguration () {
         feeds.asObservable()
-            .observeOn(MainScheduler.instance)
-            .bind(to: tableView.rx.items(cellIdentifier: "FlickrTableViewCell", cellType: FlickrTableViewCell.self)) {
-                [weak self] (index, feed, cell) in
+            .bind(to: tableView.rx.items(cellIdentifier: "FlickrTableViewCell", cellType: FlickrTableViewCell.self)) { [weak self](_, feed, cell) in
+                
                 guard let strongSelf = self else { return }
                 cell.titleLabel.text = feed.title
                 cell.mediaImageView.image = nil
-                
                 strongSelf.imageLoader.loadImage(fromUrlString: feed.mediaUrl,
-                                            httpServiceProvider: strongSelf.flickrHTTPServiceProvider)
-                    .observeOn(ConcurrentMainScheduler.instance)
+                                                 httpServiceProvider: strongSelf.flickrHTTPServiceProvider)
+                    .observeOn(MainScheduler.instance)
                     .subscribe(onNext: { image in
                         if let image = image {
                             cell.mediaImageView.image = image
+                            cell.updateConstraintsIfNeeded()
+                            cell.setNeedsUpdateConstraints()
                         }
-                    }).disposed(by: strongSelf.disposeBag)
-                
-                 cell.selectionStyle = .none
+                    })
+                    .disposed(by: strongSelf.disposeBag)
+                cell.selectionStyle = .none
             }
             .disposed(by: disposeBag)
     }
@@ -134,6 +95,46 @@ extension ViewController {
             alertController.addAction(UIAlertAction(title: "OK", style: .default))
             present(alertController, animated: true)
         }
+    }
+    
+    private func getFeeds() -> Observable<[Feed]> {
+        return flickrHTTPServiceProvider.data(fromUrlString: "https://api.flickr.com/services/feeds/photos_public.gne?tags=food&tagmode=any&format=json&nojsoncallback=1")
+            .timeout(8, scheduler: MainScheduler.instance)
+            .map { data in
+                guard let invalidData = data as? Data else {
+                    throw HTTPServiceProviderError.unhandledResponse
+                }
+                // Flickr JSON output is invalid. To make it valid, remove "\\"
+                let invalidDataStr = String(data: invalidData, encoding: .utf8)
+                let validDataStr = invalidDataStr?.replacingOccurrences(of: "\\'", with: "'")
+                guard let jsonData = validDataStr?.data(using: .utf8) else{
+                    throw HTTPServiceProviderError.unhandledResponse
+                }
+              
+                guard let json = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any] else {
+                    throw HTTPServiceProviderError.unhandledResponse
+                }
+                
+                guard let items = json ["items"] as? [Any] else {
+                    throw HTTPServiceProviderError.unhandledResponse
+                }
+                
+                var feeds = [Feed]()
+                for item in items {
+                    guard let dictionary = item as? [String : Any] else {
+                        throw HTTPServiceProviderError.unhandledResponse
+                    }
+                    let feed = Feed(dictionary: dictionary)
+                    feeds.append(feed)
+                }
+                return feeds
+            }
+            .do( //onNext: {  [weak self]  feeds in
+                //                },
+                onError: { error in
+                    print(error.localizedDescription)
+            })
+            .catchErrorJustReturn([])
     }
 }
 
